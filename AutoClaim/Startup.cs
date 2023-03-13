b1014e28bb7230a377131a8275d42e534ac97e2a
sakshidandge <69038585+sakshidandge@users.noreply.github.com>
2023-03-13 17:11:45 +0530

    Add files via upload

---
+++ AutoClaim/Startup.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoClaim.Models;
+using AutoClaim.Repository;
+using AutoMapper;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace AutoClaim
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddAutoMapper(typeof(Startup));
+            services.AddScoped<IEmployee, EmployeeRepo>();
+            services.AddScoped<IVehicle, VehicleRepo>();
+            services.AddScoped<IPolicy, PolicyRepo>();
+            services.AddScoped<IClaim, ClaimRepo>();
+            services.AddScoped<IIncident, IncidentRepo>();
+            services.AddDbContext<Insc_74052Context>(options =>
+      options.UseSqlServer(Configuration.GetConnectionString("DbConnection")));
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Version = "v1", Title = "AutoClaim", Description = "ASP.NET Core 2.0 Web API" });
+            });
+
+            services.AddCors();
+            
+
+        services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseHttpsRedirection();
+            app.UseCors(x => x
+              .AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader());
+
+            app.UseStaticFiles(new StaticFileOptions
+            {
+                FileProvider = new PhysicalFileProvider(Path.Combine(env.ContentRootPath, "Document")),
+                RequestPath = "/Document"
+            });
+            app.UseMvc();
+           app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "AutoClaim v1");
+                
+            });
+        }
+    }
+}
